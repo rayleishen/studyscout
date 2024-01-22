023a568e96bd31d6fed3ec453052fa18c3103086
rayleishen <rayleishen@gmail.com>
2024-01-21 16:13:27 -0800

    Big changes 2

---
+++ hardware/test.py
@@ -0,0 +1,12 @@
+#A99 = Set as always green
+#A14 = Biggest test table
+
+from database import add_database
+from database import edit_database
+
+location = "IKB"
+is_full = False
+
+location="IKB"
+
+edit_database("A14", location, False)
